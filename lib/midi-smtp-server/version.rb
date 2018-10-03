803defec5997ed1c49cc67aab0b2b39df8f1a489
Tom Freudenberg <tom.freudenberg@4commerce.de>
2018-10-04 01:16:00 +0200

    add: new VERSION identifier for component

---
+++ lib/midi-smtp-server/version.rb
@@ -0,0 +1,12 @@
+# A small and highly customizable ruby SMTP-Server.
+module MidiSmtpServer
+
+  module VERSION
+    MAJOR = 2
+    MINOR = 3
+    TINY = 0
+
+    STRING = [MAJOR, MINOR, TINY].compact.join('.').freeze
+  end
+
+end
