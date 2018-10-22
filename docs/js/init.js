929680d7f5b772dbd3672a8f6c1ca1fcd7dcbf84
Tom Freudenberg <tom.freudenberg@4commerce.de>
2018-10-22 02:41:46 +0200

    add: new project micro homepage

---
+++ docs/js/init.js
@@ -0,0 +1,14 @@
+(function($){
+  $(function(){
+
+    /*
+       Enable SideBar Navigation
+    */
+    $('.button-collapse').sideNav({
+      menuWidth: 240,
+      activationWidth: 70,
+      closeOnClick: true
+    });
+
+  }); // end of document ready
+})(jQuery); // end of jQuery name space
