ae57c95345c3f4684661f126c2de8d19fc301aec
Tom Freudenberg <tom.freudenberg@4commerce.de>
2020-01-18 03:26:33 +0100

    fix: leave given hosts untouched, so prevent identical ip_addresses

--- test/specs/ports-and-hosts_test.rb
+++ test/specs/ports-and-hosts_test.rb
@@ -3,15 +3,15 @@
 describe MidiSmtpServerTest do
   # initialize before tests
   before do
-    @smtpd = MidiSmtpServerTest.new(2525, '127.0.0.1, localhost, ::1')
+    @smtpd = MidiSmtpServerTest.new(2525, '127.0.0.1, ::1')
   end
 
-  describe '1 port 3 hosts' do
+  describe '1 port 2 hosts' do
     it 'must respond with [2525]' do
       @smtpd.ports.must_equal ['2525']
     end
-    it 'must respond with [127.0.0.1, localhost, ::1]' do
-      @smtpd.hosts.must_equal ['127.0.0.1', 'localhost', '::1']
+    it 'must respond with [127.0.0.1, ::1]' do
+      @smtpd.hosts.must_equal ['127.0.0.1', '::1']
     end
   end
 end
@@ -19,15 +19,15 @@ end
 describe MidiSmtpServerTest do
   # initialize before tests
   before do
-    @smtpd = MidiSmtpServerTest.new('2525, 3535', '127.0.0.1, localhost, ::1')
+    @smtpd = MidiSmtpServerTest.new('2525, 3535', '127.0.0.1, ::1')
   end
 
-  describe '2 ports 3 hosts' do
+  describe '2 ports 2 hosts' do
     it 'must respond with [2525, 3535]' do
       @smtpd.ports.must_equal ['2525', '3535']
     end
-    it 'must respond with [127.0.0.1, localhost, ::1]' do
-      @smtpd.hosts.must_equal ['127.0.0.1', 'localhost', '::1']
+    it 'must respond with [127.0.0.1, ::1]' do
+      @smtpd.hosts.must_equal ['127.0.0.1', '::1']
     end
   end
 end
@@ -35,15 +35,15 @@ end
 describe MidiSmtpServerTest do
   # initialize before tests
   before do
-    @smtpd = MidiSmtpServerTest.new('2525, 2525:3535', '127.0.0.1, localhost, ::1')
+    @smtpd = MidiSmtpServerTest.new('2525, 2525:3535', '127.0.0.1, ::1')
   end
 
-  describe '2+ ports 3 hosts' do
+  describe '2+ ports 2 hosts' do
     it 'must respond with [2525, 2525:3535]' do
       @smtpd.ports.must_equal ['2525', '2525:3535']
     end
-    it 'must respond with [127.0.0.1, localhost, ::1]' do
-      @smtpd.hosts.must_equal ['127.0.0.1', 'localhost', '::1']
+    it 'must respond with [127.0.0.1, ::1]' do
+      @smtpd.hosts.must_equal ['127.0.0.1', '::1']
     end
   end
 end
