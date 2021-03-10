7d663553e45ad90ba2e3f3400da32736e02f8d23
Tom Freudenberg <tom.freudenberg@4commerce.de>
2021-03-10 15:51:34 +0100

    fix: add also exception message to backtrace when logging fatal exceptions

--- lib/midi-smtp-server.rb
+++ lib/midi-smtp-server.rb
@@ -491,7 +491,7 @@ module MidiSmtpServer
                 # ignore this exception due to service shutdown
               rescue StandardError => e
                 # log fatal error while handling connection
-                logger.fatal(e.backtrace.join("\n"))
+                logger.fatal(e.backtrace.dup.insert(0, "#{e}").join("\n"))
               ensure
                 begin
                   # always gracefully shutdown connection.
@@ -518,7 +518,7 @@ module MidiSmtpServer
           # ignore this exception due to service shutdown
         rescue StandardError => e
           # log fatal error while starting new thread
-          logger.fatal(e.backtrace.join("\n"))
+          logger.fatal(e.backtrace.dup.insert(0, "#{e}").join("\n"))
         ensure
           begin
             # drop the service
